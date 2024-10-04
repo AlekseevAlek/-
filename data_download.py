e681ca4230e77221436908fe18bd3a8821589579
Aleksei <generatoralex@yandex.ru>
2024-10-04 17:43:58 +0300

    Задача5

--- data_download.py
+++ data_download.py
@@ -1,13 +1,14 @@
 import yfinance as yf
 import pandas as pd
-
-def fetch_stock_data(ticker, period='1mo'):
+from datetime import datetime
+def fetch_stock_data(ticker, start_date, end_date):
+    '''Функция получает исторические данные об акциях для указанного тикера и временного периода. Возвращает DataFrame с данными.'''
     stock = yf.Ticker(ticker)
-    data = stock.history(period=period)
+    data = stock.history(start=start_date, end=end_date)
     return data
 
-
 def add_moving_average(data, window_size=5):
+    '''Функция добавляет в DataFrame колонку со скользящим средним, рассчитанным на основе цен закрытия.'''
     data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
     return data
 
@@ -17,8 +18,6 @@ def calculate_and_display_average_price(data):
     data = data['Close'].mean()
     print(f"Средняя цена закрытия акций за период: {data}")
 
-
-
 def notify_if_strong_fluctuations(data, threshold=15):
     '''Функция анализирует данные и уведомляет пользователя, если цена акций колебалась более чем на заданный процент за период'''
 
@@ -26,13 +25,11 @@ def notify_if_strong_fluctuations(data, threshold=15):
     if percent >= threshold:
         print(f'цена акций колебалась на {percent}%')
 
-
 def export_data_to_csv(data, filename):
     '''Функция сохраняет загруженные данные об акциях в CSV файл.'''
 
     data = data['Close']
     data.to_csv(filename)
-
 
 def add_technical_indicators(data):
     '''Функция добавляет дополнительный технический индикатор RSI'''
@@ -45,4 +42,12 @@ def add_technical_indicators(data):
     rsi = 100 - (100 / (1 + rs))
     data['RSI'] = rsi
     return data
+
+
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,31 +1,67 @@
 import data_download as dd
 import data_plotting as dplt
 import pandas as pd
-
+from datetime import datetime, timedelta
 def main():
     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
     print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
     print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
     ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
-    period = input("Введите период для данных (например, '1mo' для одного месяца): ")
-    filename = 'DataF.csv'
+
+    '''Пользователь может ввести даты в формате YYYY-MM-DD или использовать предустановленные периоды'''
+    period_input = input(
+        "Введите период для данных (например, '2020-01-01 2020-12-31' для данных за 2020 год, или '1mo' для одного месяца): ")
+
+    '''Обработка ввода пользователя'''
+    if '-' in period_input:
+        start_date, end_date = period_input.split()
+        start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    else:
+        period_map = {
+            '1d': timedelta(days=1),
+            '5d': timedelta(days=5),
+            '1mo': pd.Timedelta(weeks=4),
+            '3mo': pd.Timedelta(weeks=12),
+            '6mo': pd.Timedelta(weeks=24),
+            '1y': pd.Timedelta(weeks=52),
+            '2y': pd.Timedelta(weeks=104),
+            '5y': pd.Timedelta(weeks=260),
+            '10y': pd.Timedelta(weeks=520),
+            'since begin of year': datetime.now().replace(month=1, day=1),
+            'max': None
+        }
+
+        if period_input == 'max':
+            start_date = datetime.min
+            end_date = datetime.max
+        elif period_input in period_map:
+            start_date = datetime.now() - period_map[period_input]
+            end_date = datetime.now()
 
     # Fetch stock data
-    stock_data = dd.fetch_stock_data(ticker, period)
-
+    stock_data = dd.fetch_stock_data(ticker, start_date, end_date)
     # Add moving average to the data
     stock_data = dd.add_moving_average(stock_data)
 
     dd.add_technical_indicators(stock_data)
 
-    dplt.create_and_save_plot(stock_data, ticker, period)
+    dplt.create_and_save_plot(stock_data, ticker, period_input)
 
     dd.calculate_and_display_average_price(stock_data)
 
     dd.notify_if_strong_fluctuations(stock_data)
 
+    filename = 'DataF.csv'
     dd.export_data_to_csv(stock_data, filename)
+
+
+
+
+
+
+
 
 
 
